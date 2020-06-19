527b9f64ea23ec0a218c1ff6bbf1e1872a06197f
sohan110111 <sohan110111@gmail.com>
2020-06-19 23:17:10 +0600

    function return value discussion

--- function_01.js
+++ function_01.js
@@ -1,33 +1,35 @@
+
+
 function evenify (num) {
-    for (let index = 0; index < num.length; index++) {
-        const element = num[index];
-        if (element % 2 == 0) {
-            console.log(element, ': is even.');
-        } else {
-            console.log(element * 2 , ': is even.');
-        }
-    }
-    
-    /* if (num % 2 == 0) {
-        console.log(num, ': is even.');
+    var result;
+    if (num % 2 == 0) {
+        result = num;
     } else {
-        console.log(num , ': is odd.');
-    } */
+        result = num + " is odd";
+    }
+
+    return result;
 }
-/* console.log(12);
-console.log(); */
+var result = evenify(13);
+console.log(result);
 
-nums = [5, 12, 89, 45, 18, 8];
-/* for (let i = 0; i < nums.length; i++) {
-    const num = nums[i]; */
-    //console.log(num * 2);
-    //evenify (num);
-// }
-evenify(nums);
+function square (num) {
+    return num * num;
+}
 
-friends_age = [13, 17, 19, 20, 18];
-//for (let i = 0; i < friends_age.length; i++) {
-    //const age = friends_age[i];
-    //evenify (age); 
-//}
-evenify(friends_age);
+var squareResult = square(12);
+console.log("Square is ", squareResult);
+console.log("Square Root is : ", Math.sqrt(squareResult));
+
+var nums = [1, 2, 3, 4, 5];
+function double (value) {
+    var double = [];
+    for (let index = 0; index < value.length; index++) {
+        const element = value[index] * 2;
+        double.push(element);
+    }
+
+    return double;
+} 
+
+console.log(double(nums));
