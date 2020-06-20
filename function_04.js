3f1512eaba410eec5d8a4ce5eacc74394fba52bd
sohan110111 <sohan110111@gmail.com>
2020-06-20 11:18:34 +0600

    How to organize code inside function

---
+++ function_04.js
@@ -0,0 +1,20 @@
+function addNumbers (num1, num2) {
+    console.log(arguments); // [Arguments] { '0': 3, '1': 4, '2': 5, '3': 6 }
+    console.log(arguments[3]); // 3
+
+    var sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        const element = arguments[i];
+        sum = sum + element;
+    }
+    return sum;
+}
+
+var result = addNumbers(3, 4, 5, 6);
+
+console.log(result);
+
+function logInfo(message) {
+    console.log(message);
+}
+logInfo("Good morning."); 
